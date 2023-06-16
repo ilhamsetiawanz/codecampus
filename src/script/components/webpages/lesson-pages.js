0cc3fd42ec0ef2061ddad821c2ce5a78e2f31b7b
Boy Boy Geodevandry <d1041201027@student.untan.ac.id>
2023-06-17 00:49:47 +0700

    update patch lesson Baru

--- src/script/components/webpages/lesson-pages.js
+++ src/script/components/webpages/lesson-pages.js
@@ -20,9 +20,12 @@ export class LessonPage extends HTMLElement {
             <h1 class="display-4 fs-1 fw-bolder text-white">${courses.tittle}</h1>
             <p class="lead fs-6 text-white">Jangan pernah berhenti belajar. Di dunia coding, ada selalu hal baru yang bisa dipelajari dan dikuasai.<br> Teruslah eksplorasi dan berani mengambil tantangan baru.</p>
           </div>
-        </div>      
+        </div> 
+        <div class="desc">
+        <h2>Pendahuluan</h2>     
         <p>${courses.desc}<p>
-        <div class="materi ">
+        </div>
+        <div class="materi">
           <h2>Materi</h2>
         </div>
       `;
@@ -32,7 +35,9 @@ export class LessonPage extends HTMLElement {
       courses.materi.forEach((materi) => {
         const materiItem = document.createElement('div');
         materiItem.innerHTML = `
-          <a href="#/classroom/${materi.classId}"><strong>Title:</strong> ${materi.title}</a>
+          <a href="#/classroom/${materi.classId}"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 w-6 dark:text-gray-500 text-gray-400 group-hover:text-gray-500 dark:group-hover:text-gray-400 shrink-0 svg-small"><title>Lesson</title>
+          <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"></path>
+        </svg> ${materi.title}</a>
         `;
         materiContainer.appendChild(materiItem);
       });
@@ -40,7 +45,7 @@ export class LessonPage extends HTMLElement {
       courses.projects.forEach((project) => {
         const projectsItem = document.createElement('div');
         projectsItem.innerHTML = `
-          <a href="#/classroom/${project.id}"><strong>Title:</strong> ${project.title}</a>
+          <a href="#/classroom/${project.id}" ${project.title}</a>
         `;
         materiContainer.appendChild(projectsItem);
       });
